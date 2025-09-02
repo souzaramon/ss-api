2a54c9eb6baa146fd4868926497407d4ac25f6da
souzaramon <hi.souzaramon@gmail.com>
2025-09-02 19:21:19 -0300

    feat: setup gin, ginzap

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,14 +1,56 @@
 package main
 
 import (
+	"context"
+	"net"
+	"net/http"
+	"time"
+
 	"go.uber.org/fx"
 	"go.uber.org/zap"
+
+	ginzap "github.com/gin-contrib/zap"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fx.New(
 		fx.Provide(
+			NewGinApp,
 			zap.NewProduction,
 		),
+		fx.Invoke(func(*gin.Engine) {}),
 	).Run()
+}
+
+func NewGinApp(lc fx.Lifecycle, log *zap.Logger) *gin.Engine {
+	g := gin.New()
+
+	g.RedirectTrailingSlash = false
+
+	g.Use(ginzap.Ginzap(log, time.RFC3339, true))
+	g.Use(ginzap.RecoveryWithZap(log, true))
+
+	server := &http.Server{Addr: ":8080", Handler: g}
+
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", server.Addr)
+			if err != nil {
+				return err
+			}
+
+			go server.Serve(ln)
+
+			log.Info("Starting HTTP server", zap.String("addr", server.Addr))
+
+			return nil
+
+		},
+		OnStop: func(ctx context.Context) error {
+			return server.Shutdown(ctx)
+		},
+	})
+
+	return g
 }
